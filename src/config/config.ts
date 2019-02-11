e8fffdcfa4a05a0dde5aff64b5f735c508cb4e46
unknown <hhemaidia@myapphone.com>
2019-02-11 10:12:32 +0100

    update default config

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 let config;
 export default config = {
-  env: process.env.NODE_ENV === 'development' ? 'development' : 'production',
-  port: process.env.PORT,
+  env: (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 'development' : 'production',
+  port: process.env.PORT || '3000',
 
   db: {
     mongodb: 'mongodb://localhost/expressts'
@@ -12,6 +12,6 @@ export default config = {
 
   limiter: {
     ms: 15 * 60 * 1000, // 15 minutes
-    max: 10, 
+    max: 10,
   }
 }
